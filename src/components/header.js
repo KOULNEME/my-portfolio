84541a52a3a7d9240a5f02b468257e1a0436560d
KOULBEME KOSSI <kossikoulbeme2@gmail.com>
2025-08-23 03:25:07 +0530

    add withPrefix

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { withPrefix } from 'gatsby';
 
 const Nav = styled.nav`
   display: flex;
@@ -40,7 +41,7 @@ const Header = () => {
   return (
     <Nav>
       <Link to="/">
-        <Logo src="/kossi-logo.png" alt="Logo" />
+        <Logo src={withPrefix("/kossi-logo.png")} alt="Logo" />
       </Link>
       <NavLinks>
         <StyledLink to="/about">About</StyledLink>
